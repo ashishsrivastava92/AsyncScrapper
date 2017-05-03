1b92875e51898ce9a0751158304c294f393101ea
voris-dev-4 <ashish@voris.co.in>
2017-05-03 17:04:54 +0530

    Class based scrapper, url pooling and csv writer via gevent.spawn

--- csv_reader.py
+++ csv_reader.py
@@ -6,7 +6,7 @@ cd = os.path.dirname(os.path.abspath(__file__))     #Current Directory
 
 # Generator for retrieving urls
 def csv_url_reader(filename):
-    with open(filename, "rb") as csv_file:
+    with open(filename, "r") as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
             try:
@@ -17,14 +17,10 @@ def csv_url_reader(filename):
 
 def csv_writer(url, accept=True):
     if accept:
-        with open(os.path.join(cd, 'accepted.csv'), 'a') as accepted:
+        with open(os.path.join(cd, 'generated_csv/accepted.csv'), 'a') as accepted:
             writer = csv.writer(accepted)
             writer.writerow([url])
     else:
-        with open(os.path.join(cd, 'rejected.csv'), 'a') as accepted:
+        with open(os.path.join(cd, 'generated_csv/rejected.csv'), 'a') as accepted:
             writer = csv.writer(accepted)
             writer.writerow([url])
-
-if __name__ == '__main__':
-    for i in csv_url_reader():
-        print i
--- scraper_test.py
+++ scraper_test.py
@@ -1,52 +1,58 @@
-import gevent
-from gevent import monkey, pool
+from gevent import monkey, pool, spawn, joinall, sleep
 monkey.patch_all()
-from timeit import timeit
+
+# from timeit import timeit
+# from socket import timeout
 from urllib2 import urlopen, HTTPError, URLError
+from httplib import BadStatusLine
 from bs4 import BeautifulSoup
-from csv_reader import csv_url_reader, csv_writer
-
-jobs = []
-links = []
-p = pool.Pool(10)
+from csv_utils import csv_url_reader, csv_writer
 
 
-def get_links(url):
-    try:
-        page = urlopen(url, timeout=5)      # Raise URLError for timeout, indefinite wait problem
-    except (URLError, HTTPError):
-        print url, '-', "Invalid Url/ Unauthorized to scrape"
-        return
-    except Exception as e:
-        from traceback import format_exc
-        print url, '-', "Exception :", format_exc().splitlines()[-1]
-        return
-    else:
-        if page.code == 200:
-            soup = BeautifulSoup(page.read(), "lxml")
-            sources = soup.findAll('script', {"src": True})
-            for source in sources:
-                print source['src']
-                if 'dcmegamenu.1.2.js' in source['src']:
-                    # csv_writer(url)     # add to accepted.csv
-                    jobs.append(gevent.spawn(csv_writer, url))
-                    return
-            # csv_writer(url, accept=False)       # add to rejected.csv
-            jobs.append(gevent.spawn(csv_writer, url, accept=False))
-        return
+class Scraper:
+    def __init__(self, file_name):
+        self.filename = file_name
+        self.jobs = []
+        self.pool = pool.Pool(20)
 
+    def get_links(self, url):
+        try:
+            page = urlopen(url, timeout=5)
+        except (URLError, HTTPError):
+            # self.jobs.append(spawn(csv_writer, url, accept=False))     # Can add to rejected list if wanted
+            print url, '-', "Url Error/ Unauthorized to scrape"
+            return
+        # except timeout:
+        #     print url, ':', "Url TimeOut"
+        except ValueError:
+            print url, ':', "Invalid Url Type"
+        except BadStatusLine:
+            print url, '-', "Requires User Agent data"
+        except Exception, e:
+            from traceback import format_exc
+            print url, '-', "Exception :", format_exc().splitlines()[-1]
+            return
+        else:
+            if page.code == 200:
+                soup = BeautifulSoup(page.read(), "lxml")
+                sources = soup.findAll('script', {"src": True})
+                for source in sources:
+                    if 'jquery.js' in source['src']:
+                        self.jobs.append(spawn(csv_writer, url))
+                        return
+                self.jobs.append(spawn(csv_writer, url, accept=False))       # add to rejected.csv
+            return
 
-def job(filename):
-    # from sys import argv
-    # try:
-    # filename = argv[1]
-    for link in csv_url_reader(filename):
-        jobs.append(gevent.spawn(get_links, link))
-    gevent.joinall(jobs)
-    # if len(argv) > 2:
-    #     raise IndexError
-    # except IndexError:
-    #     print 'provide csv file'
+    def job(self):
+        # TODO: Implement bloom filter in csv_url_reader() for million records, If duplicate url don't create greenlet
+        # For 100 entries hash map won't provide much improvement
+
+        for link in csv_url_reader(self.filename):
+            self.pool.spawn(self.get_links, link)
+            # if p1.full():
+            #     sleep(.2)
+        self.pool.join(timeout=10)      # url request in pool
+        joinall(self.jobs)      # Csv writer with gevent spawn
 
 
 def wrapper(func, *args, **kwargs):
@@ -57,34 +63,16 @@ def wrapper(func, *args, **kwargs):
 
 if __name__ == '__main__':
     from sys import argv
-    filename = argv[1]
-    wrapped = wrapper(job, filename)
-    a = timeit(wrapped, number=1)
-    print a
-    # from sys import argv
-    # try:
-    #     filename = argv[1]
-    #     for link in csv_url_reader(filename):
-    #         jobs.append(gevent.spawn(get_links, link))
-    #     gevent.joinall(jobs)
-    #     if len(argv) > 2:
-    #         raise IndexError
-    # except IndexError:
-    #     print 'provide csv file'
-
-# def job(URLS):
-#     for url in URLS:
-#         jobs.append(p.spawn(get_links, url))
-#     gevent.joinall(jobs)
-#     return
-
-# def wrapper(func, *args, **kwargs):
-#     def wrapped():
-#         return func(*args, **kwargs)
-#     return wrapped
-
-# urls.extend(a)
-# wrapped = wrapper(job, urls)
-# # job(urls)
-# a = timeit.timeit(wrapped, number=1)
-# print a
+    try:
+        filename = argv[1]
+        scraper = Scraper(filename)
+        # wrapped = wrapper(scraper.job)
+        # timing = timeit(wrapped, number=1)
+        # print timing
+        scraper.job()
+        if len(argv) > 2:
+            raise IndexError
+    except IndexError:
+        print 'provide csv file'
+    except Exception, e:
+        print "Something went wrong... try again"
